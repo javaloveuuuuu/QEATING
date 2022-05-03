5eee200032d3f8c2c1cc97768fed32d3a0ffc832
Hopenononobug <100891331+Hopenononobug@users.noreply.github.com>
2022-05-03 22:26:43 +0800

    Add files via upload

---
+++ demo/pages/wucan/wucan.js
@@ -0,0 +1,72 @@
+// pages/zaocan.js
+Page({
+  gotoPage: function (event) {
+    const name = event.target.id;//1或者2得到点击了按钮1或者按钮2 
+    const url = "/pages/"+name+"/"+name ;//得到页面url 
+    wx.navigateTo({ 
+    url: url,  
+}) 
+},
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
